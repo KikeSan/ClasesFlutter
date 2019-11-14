3cb415d1b7537cc3ec4c37e096a9592b34745368
Kike Sanchez <kikin1601@hotmail.com>
2019-11-14 12:03:34 -0500

    Clase 112: Pasar arguments entre paginas con PushNamed

--- peliculas/lib/src/widgets/movie_horizontal.dart
+++ peliculas/lib/src/widgets/movie_horizontal.dart
@@ -34,7 +34,7 @@ class MovieHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula) {
-    return Container(
+    final tarjeta = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: <Widget>[
@@ -57,9 +57,17 @@ class MovieHorizontal extends StatelessWidget {
         ],
       ),
     );
+
+    return GestureDetector(
+      child: tarjeta,
+      onTap: () {
+        Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+        print('ID: ${pelicula.title}');
+      },
+    );
   }
 
-  List<Widget> _tarjetas(BuildContext context) {
+  /* List<Widget> _tarjetas(BuildContext context) {
     return peliculas.map((pelicula) {
       return Container(
         margin: EdgeInsets.only(right: 15.0),
@@ -85,5 +93,5 @@ class MovieHorizontal extends StatelessWidget {
         ),
       );
     }).toList();
-  }
+  } */
 }
