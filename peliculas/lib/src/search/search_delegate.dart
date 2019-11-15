8cd74a42b0c6d2471b18f7166dcaf53a908cfb80
Kike Sanchez <kikin1601@hotmail.com>
2019-11-15 09:51:50 -0500

    Clase 121: BuildActions y BuildLeading

--- peliculas/lib/src/search/search_delegate.dart
+++ peliculas/lib/src/search/search_delegate.dart
@@ -4,24 +4,39 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro appbar ejm: limpiar, cancelar
-    return null;
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // Icono a la izq del appbar
-    return null;
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados q vamos a mostrar
-    return null;
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Las sugerencias que aparecen cuando la persona escribe
-    return null;
+    return Container();
   }
 }
