8b40b06bbbf0cbea360dd1ee327dfb737f534b48
Kike Sanchez <kikin1601@hotmail.com>
2019-11-15 09:34:15 -0500

    Clase 120: Search Delegate

---
+++ peliculas/lib/src/search/search_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro appbar ejm: limpiar, cancelar
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izq del appbar
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados q vamos a mostrar
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Las sugerencias que aparecen cuando la persona escribe
+    return null;
+  }
+}
