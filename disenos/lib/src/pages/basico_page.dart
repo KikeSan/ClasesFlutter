76ae7206fef678171c1b9271ee5663d5ce1f15f6
Kike Sanchez <kikin1601@hotmail.com>
2019-11-15 12:31:08 -0500

    Curso 131: Diseño básico parte 3

--- disenos/lib/src/pages/basico_page.dart
+++ disenos/lib/src/pages/basico_page.dart
@@ -7,50 +7,60 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-      ],
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+      ),
     ));
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          'https://images.pexels.com/photos/247478/pexels-photo-247478.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/247478/pexels-photo-247478.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 250.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            //expande el contenido hacia sus extremos
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Montañas empinadas', style: estiloTitulo),
-                SizedBox(height: 5.0),
-                Text('Un día normal en el ocaso', style: estiloSubtitulo),
-              ],
+    return SafeArea(
+      // prevee el espacio a la izquierda de algunos dispositivos que tienen notch
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              //expande el contenido hacia sus extremos
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Montañas empinadas', style: estiloTitulo),
+                  SizedBox(height: 5.0),
+                  Text('Un día normal en el ocaso', style: estiloSubtitulo),
+                ],
+              ),
             ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red,
-            size: 30.0,
-          ),
-          Text('41', style: TextStyle(fontSize: 20.0))
-        ],
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
       ),
     );
   }
