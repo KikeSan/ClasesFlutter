b8a8919f076948906ca507de90e10a71c1ec8ed6
Kike Sanchez <kikin1601@hotmail.com>
2019-11-24 11:37:23 -0500

    Fotos de actor - swipper

--- peliculas/lib/src/providers/peliculas_provider.dart
+++ peliculas/lib/src/providers/peliculas_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/actuaEn_model.dart';
+import 'package:peliculas/src/models/fotosActor_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -90,6 +91,17 @@ class PeliculasProvider {
     //print('provider');
     //print(actuaEnPeli.actuaEn);
     return actuaEnPeli.actuaEn;
+  }
+
+  Future<List<Fotos>> getFotosPerfil(String actorId) async {
+    final url = Uri.https(_url, '3/person/$actorId/images', {
+      'api_key': _apikey,
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final fotosPerfil = new FotosActor.fromJsonList(decodedData['profiles']);
+    return fotosPerfil.fotos;
   }
 
   Future<List<Pelicula>> buscarPelicula(String query) async {
