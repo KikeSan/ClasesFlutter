48eefa84d35ebc328e3891afec5f1c298962bbd3
Kike Sanchez <kikin1601@hotmail.com>
2019-11-25 09:30:22 -0500

    Clase 135: Diseño con scroll - parte 2

--- disenos/lib/src/pages/scroll_page.dart
+++ disenos/lib/src/pages/scroll_page.dart
@@ -12,8 +12,49 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Página 111'),
+    return Stack(
+      //para que esten apilados uno encima de otro
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0),
+          Text('11°', style: estiloTexto),
+          Text('Miércoles', style: estiloTexto),
+          Expanded(
+              child:
+                  Container()), //estira el ancho a lo mas posible considerando los elementos restantes
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+        ],
+      ),
     );
   }
 
