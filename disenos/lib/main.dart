7700e39fc6fc73faa0877609b3fdda315ade0660
Kike Sanchez <kikin1601@hotmail.com>
2019-11-25 10:30:02 -0500

    Clase 138 - Statusbar y titulos

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:disenos/src/pages/botones_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:disenos/src/pages/botones_page.dart';
 import 'package:disenos/src/pages/basico_page.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
 
@@ -8,6 +9,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Este codigo comentado sirve para poner el texto blanco del status bar del iphone, donde sale el indicador de bateria arriba
+    /* SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.white)); */
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
--- disenos/lib/src/pages/botones_page.dart
+++ disenos/lib/src/pages/botones_page.dart
@@ -9,6 +9,13 @@ class BotonesPage extends StatelessWidget {
         body: Stack(
       children: <Widget>[
         _fondoApp(),
+        SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _titulos(),
+            ],
+          ),
+        )
       ],
     ));
   }
@@ -52,4 +59,31 @@ class BotonesPage extends StatelessWidget {
       ],
     );
   }
+
+  Widget _titulos() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0), //padding en todas las direcciones
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Classify transaction',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Classify this transaction into a particular category',
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //sss
 }
