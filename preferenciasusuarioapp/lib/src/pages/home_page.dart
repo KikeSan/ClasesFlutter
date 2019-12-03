f6eb2c07784e4f2af601381fb817268a75d59d48
Kike Sanchez <kikin1601@hotmail.com>
2019-12-03 09:51:24 -0500

    Clase 181: Preferencias de usuario

---
+++ preferenciasusuarioapp/lib/src/pages/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/pages/settings_page.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de Usuario'),
+      ),
+      drawer: _crearMenu(context),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario:'),
+          Divider(),
+          Text('Género:'),
+          Divider(),
+          Text('Nombre de usuario:'),
+        ],
+      ),
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero, //PARA QUE SE PEGUE AL BORDE SUPERIOR
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/menu-img.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: Icon(Icons.pages, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Party Mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+              leading: Icon(Icons.settings, color: Colors.blue),
+              title: Text('Settings'),
+              onTap: () => Navigator.pushReplacementNamed(
+                  context, SettingsPage.routeName) //PARA CAMBIAR LA RAIZ
+              )
+        ],
+      ),
+    );
+  }
+}
