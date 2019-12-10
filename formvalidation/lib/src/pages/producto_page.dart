2d38a3d37f47e38b4dddd1d9412c4654ff0cf909
Kike Sanchez <kikin1601@hotmail.com>
2019-12-10 10:45:41 -0500

    Clase 213: Provider de productos

--- formvalidation/lib/src/pages/producto_page.dart
+++ formvalidation/lib/src/pages/producto_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
 import 'package:formvalidation/src/utils/utils.dart' as utils;
 
 class ProductoPage extends StatefulWidget {
@@ -9,6 +10,7 @@ class ProductoPage extends StatefulWidget {
 
 class _ProductoPageState extends State<ProductoPage> {
   final formKey = GlobalKey<FormState>();
+  final productoProvider = new ProductosProvider();
 
   ProductoModel producto = new ProductoModel();
 
@@ -112,5 +114,7 @@ class _ProductoPageState extends State<ProductoPage> {
     print(producto.titulo);
     print(producto.valor);
     print(producto.disponible);
+
+    productoProvider.crearProducto(producto);
   }
 }
---
+++ formvalidation/lib/src/providers/productos_provider.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductosProvider {
+  final String _url = 'https://flutter-varios-c8cab.firebaseio.com';
+  Future<bool> crearProducto(ProductoModel producto) async {
+    final url = '$_url/productos.json';
+    final resp = await http.post(url, body: productoModelToJson(producto));
+    final decodedData = json.decode(resp.body);
+    print(decodedData);
+    return true;
+  }
+}
