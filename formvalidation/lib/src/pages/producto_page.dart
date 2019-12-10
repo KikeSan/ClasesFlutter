52ac1d79f52b96e12fd0fa51b58b0305a70bfb47
Kike Sanchez <kikin1601@hotmail.com>
2019-12-10 10:17:10 -0500

    Clase 211: Conectar formulario con el modelo de producto

--- formvalidation/lib/src/pages/producto_page.dart
+++ formvalidation/lib/src/pages/producto_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
 import 'package:formvalidation/src/utils/utils.dart' as utils;
 
 class ProductoPage extends StatefulWidget {
@@ -8,6 +9,8 @@ class ProductoPage extends StatefulWidget {
 
 class _ProductoPageState extends State<ProductoPage> {
   final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel();
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +34,12 @@ class _ProductoPageState extends State<ProductoPage> {
           child: Form(
             key: formKey,
             child: Column(
-              children: <Widget>[_crearNombre(), _crearPrecio(), _crearBoton()],
+              children: <Widget>[
+                _crearNombre(),
+                _crearPrecio(),
+                _crearDisponible(),
+                _crearBoton()
+              ],
             ),
           ),
         ),
@@ -41,8 +49,11 @@ class _ProductoPageState extends State<ProductoPage> {
 
   Widget _crearNombre() {
     return TextFormField(
+      //traigo los valores por defecto del modelo producto
+      initialValue: producto.titulo,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(labelText: 'Producto'),
+      onSaved: (value) => producto.titulo = value,
       validator: (value) {
         if (value.length < 3) {
           return 'Ingrese el nombre del producto';
@@ -55,8 +66,11 @@ class _ProductoPageState extends State<ProductoPage> {
 
   Widget _crearPrecio() {
     return TextFormField(
+      //traigo los valores por defecto del modelo producto
+      initialValue: producto.valor.toString(),
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(labelText: 'Precio'),
+      onSaved: (value) => producto.valor = double.parse(value),
       validator: (value) {
         if (utils.isNumeric(value)) {
           return null;
@@ -64,6 +78,17 @@ class _ProductoPageState extends State<ProductoPage> {
           return 'Solo números';
         }
       },
+    );
+  }
+
+  Widget _crearDisponible() {
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'),
+      activeColor: Colors.deepPurple,
+      onChanged: (value) => setState(() {
+        producto.disponible = value;
+      }),
     );
   }
 
@@ -81,6 +106,11 @@ class _ProductoPageState extends State<ProductoPage> {
   void _submit() {
     //Si el formulario no es valido se queda en esta linea y no continua con el resto de codigo
     if (!formKey.currentState.validate()) return;
+    //Esta instrucción va a disparar todos los save q tiene el formulario pejm: onSaved: (value) => producto.titulo = value,
+    formKey.currentState.save();
     print('Todo ok');
+    print(producto.titulo);
+    print(producto.valor);
+    print(producto.disponible);
   }
 }
