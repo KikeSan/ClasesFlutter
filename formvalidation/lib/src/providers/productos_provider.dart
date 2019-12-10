fcaa275e43677c0115ec796800dca50612b7a9f6
Kike Sanchez <kikin1601@hotmail.com>
2019-12-10 11:03:35 -0500

    clase 214: Cargar productos de firebase

--- formvalidation/lib/src/providers/productos_provider.dart
+++ formvalidation/lib/src/providers/productos_provider.dart
@@ -6,10 +6,29 @@ import 'package:http/http.dart' as http;
 class ProductosProvider {
   final String _url = 'https://flutter-varios-c8cab.firebaseio.com';
   Future<bool> crearProducto(ProductoModel producto) async {
+    //agregar el .json al final de la url para ver la estructura del json
     final url = '$_url/productos.json';
     final resp = await http.post(url, body: productoModelToJson(producto));
     final decodedData = json.decode(resp.body);
     print(decodedData);
     return true;
+  }
+
+  Future<List<ProductoModel>> cargarProductos() async {
+    final url = '$_url/productos.json';
+    final resp = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+    final List<ProductoModel> productos = new List();
+    //print(decodedData);
+    if (decodedData == null) return [];
+
+    decodedData.forEach((id, prod) {
+      final prodTemp = ProductoModel.fromJson(prod);
+      //agregamos el id que no se encuentra en el mapa prod sino afuera en un nivel superior
+      prodTemp.id = id;
+      productos.add(prodTemp);
+    });
+    print(productos);
+    return productos;
   }
 }
