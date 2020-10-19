0bacaac58eb11240d8154acae6937787a78d2994
Kike Sanchez <kikin1601@hotmail.com>
2020-10-19 07:40:03 -0500

    :phone: Implementando el BottomNavigationBar

--- newsapp/lib/src/pages/tabs_pages.dart
+++ newsapp/lib/src/pages/tabs_pages.dart
@@ -4,9 +4,31 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(currentIndex: 0, items: [
+      BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline), title: Text('Para ti')),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.public), title: Text('Encabezados')),
+    ]);
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: BouncingScrollPhysics(), //Esto es para no mostrar una curva al borde cuando ya no hay más tabs
       physics:
-          BouncingScrollPhysics(), //Esto es para no mostrar una curva al borde cuando ya no hay más tabs
+          NeverScrollableScrollPhysics(), //Esto es para no navegar entre tabs como slide
       children: <Widget>[
         Container(
           color: Colors.red,
@@ -15,6 +37,6 @@ class TabsPage extends StatelessWidget {
           color: Colors.green,
         )
       ],
-    ));
+    );
   }
 }
