cbc83e3fb8a5094439d96d67d1811760264e5463
Kike Sanchez <kikin1601@hotmail.com>
2020-10-22 07:46:09 -0500

    - Cambiar color de la categoría seleccionada

--- newsapp/lib/src/pages/tab2_page.dart
+++ newsapp/lib/src/pages/tab2_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/theme/tema.dart';
 import 'package:provider/provider.dart';
 
 class Tab2Page extends StatelessWidget {
@@ -49,10 +50,13 @@ class _CategoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //aquí no se pone listen:false porque si se va a redibujar
+    final newService = Provider.of<NewService>(context);
+
     return GestureDetector(
       onTap: () {
         //print('Hola ${categoria.name}');
-        // Cuando el Provider se usa en un Tap o un evento se pone listen:false sino bota error
+        // Cuando el Provider se usa en un Tap o un evento se pone listen:false sino bota error porque va a tratar de redibujarlo
         final newService = Provider.of<NewService>(context, listen: false);
         newService.selectedCategory = categoria.name;
       },
@@ -60,10 +64,17 @@ class _CategoryButton extends StatelessWidget {
         width: 40,
         height: 40,
         margin: EdgeInsets.symmetric(horizontal: 10),
-        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        //padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: (newService.selectedCategory == this.categoria.name)
+                ? miTema.accentColor
+                : Colors.white),
         child: Icon(
           categoria.icon,
-          color: Colors.black54,
+          color: (newService.selectedCategory == this.categoria.name)
+              ? Colors.white
+              : Colors.black54,
         ),
       ),
     );
