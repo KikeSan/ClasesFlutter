c6b57f1c7505acff56fecfa0da18682f71eff8f3
Kike Sanchez <kikin1601@hotmail.com>
2020-10-28 06:56:16 -0500

    Mapbox Studio - Cambiar estilo a nuestro mapa

--- mapbox_mapas/lib/src/pages/fullscreenmap.dart
+++ mapbox_mapas/lib/src/pages/fullscreenmap.dart
@@ -8,6 +8,10 @@ class FullScreenMap extends StatefulWidget {
 
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapController;
+  final center = LatLng(-12.075128, -77.053708);
+  final streetStyle = 'mapbox://styles/kikesan/ckgtbuwr72k0f19lk9p05izt7';
+  final darkStyle = 'mapbox://styles/kikesan/ckgtbsfpt2jvq19mex71n2u9j';
+  String selectedStyle = 'mapbox://styles/kikesan/ckgtbsfpt2jvq19mex71n2u9j';
 
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
@@ -16,10 +20,29 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MapboxMap(
+      body: crearMapa(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+              child: Icon(Icons.map),
+              onPressed: () {
+                selectedStyle == darkStyle
+                    ? selectedStyle = streetStyle
+                    : selectedStyle = darkStyle;
+
+                setState(() {});
+              })
+        ],
+      ),
+    );
+  }
+
+  MapboxMap crearMapa() {
+    return MapboxMap(
+      styleString: selectedStyle,
       onMapCreated: _onMapCreated,
-      initialCameraPosition: const CameraPosition(
-          target: LatLng(-12.075128, -77.053708), zoom: 14),
-    ));
+      initialCameraPosition: CameraPosition(target: center, zoom: 14),
+    );
   }
 }
